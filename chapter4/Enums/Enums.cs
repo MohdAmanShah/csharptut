fdd19dd2f9660ede640ae5e23fbc51bf72fe656c
Mohd Aman Shah <reach.mohdamanshah@gmail.com>
2024-09-23 04:41:13 +0530

    Merge branch 'main' of https://github.com/MohdAmanShah/csharptut

--- chapter4/Enums/Enums.cs
+++ chapter4/Enums/Enums.cs
@@ -1,4 +1,40 @@
 public partial class Program
 {
-    
+    enum EmpTypeEnum : byte
+    {
+        Manager = 1,
+        Grunt = 2,
+        Contractor = 4,
+        VicePresident = 8
+    }
+    private static void AskForBonus(EmpTypeEnum e)
+    {
+        switch (e)
+        {
+            case EmpTypeEnum.Manager:
+                Console.WriteLine("Want stocks instead?");
+                break;
+            case EmpTypeEnum.Grunt:
+                Console.WriteLine("are you kidding!");
+                break;
+            case EmpTypeEnum.Contractor:
+                Console.WriteLine("You have enough cash.");
+                break;
+            case EmpTypeEnum.VicePresident:
+                Console.WriteLine("All right.");
+                break;
+        }
+    }
+
+    private static void EvaluateEnum(Enum e)
+    {
+        Console.WriteLine("Enum type name: {0}.", e.GetType().Name);
+        Console.WriteLine("Underlying type: {0}.", Enum.GetUnderlyingType(e.GetType()));
+        Array enumData = Enum.GetValues(e.GetType());
+        Console.WriteLine("Number of values: {0}", enumData.Length);
+        for (int i = 0; i < enumData.Length; i++)
+        {
+            Console.WriteLine($"{enumData.GetValue(i):D}: {enumData.GetValue(i)}");
+        }
+    }
 }
--- chapter4/Enums/Program.cs
+++ chapter4/Enums/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿EmpTypeEnum emp = EmpTypeEnum.Contractor;
+AskForBonus(emp);
+Console.WriteLine("Enum type: {0}.", emp.GetType().Name);
+Console.WriteLine("Underlying data store type: {0}.", Enum.GetUnderlyingType(emp.GetType()));
+Console.WriteLine("emp value: {0}.", emp.ToString());
+Console.WriteLine("emp underlying value: {0}.", (byte)emp);
+EvaluateEnum(emp);
+
+Console.Clear();
+EmpTypeEnum e1 = new EmpTypeEnum();
+DayOfWeek d = new DayOfWeek();
+ConsoleColor c = new ConsoleColor();
+
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+EvaluateEnum(e1);
+EvaluateEnum(d);
+EvaluateEnum(c);
+
+Console.Clear();
+Console.WriteLine(Convert.ToString(Int32.MinValue));
+Console.WriteLine(Convert.ToString(Int32.MinValue, 2));
+Console.WriteLine(Convert.ToString(Int32.MaxValue));
+Console.WriteLine(Convert.ToString(Int32.MaxValue, 2));
