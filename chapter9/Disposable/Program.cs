7c655dc6179284c61b27308bfacd93c71df51cd0
Mohd Aman Shah <reach.mohdamanshah@gmail.com>
2024-10-23 05:05:30 +0530

    chapter 9 done

---
+++ chapter9/Disposable/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+
+using (SomeClass s = new SomeClass(), c = new SomeClass())
+{
+    
+}
+class SomeClass : IDisposable
+{
+    public void Dispose()
+    {
+        WriteLine("Hello, world");
+    }
+}
+
+class Congres : IDisposable
+{
+    public void Do()
+    {
+        Console.WriteLine("he haw");
+    }
+    public void Dispose()
+    {
+        Console.WriteLine("no interface");
+    }
+}
--- chapter9/objectlifetime/Program.cs
+++ chapter9/objectlifetime/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿while (true)
+{
+    new SomeClass("A", 2);
+}
+Console.ReadKey();
+
+
+class SomeClass : Object
+{
+    public string A { get; set; }
+    public int B { get; set; }
+    public SomeClass(string a, int b)
+    {
+        A = a;
+        B = b;
+    }
+    ~SomeClass()
+    {
+        Console.Beep();
+    }
+}
