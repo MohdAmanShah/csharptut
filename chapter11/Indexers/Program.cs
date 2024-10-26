80d35ae961ec3d2d4746e6dde2d45d9482001f79
Mohd Aman Shah <reach.mohdamanshah@gmail.com>
2024-10-26 11:41:40 +0530

    started chapter 11 - Indexers, operator overloading, pointers types, common type conversion, extension methods, anonymouse methods,

---
+++ chapter11/Indexers/Program.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Data;
+using System.Security.Cryptography;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Clear();
+        Methods.TwoDArrayExample();
+    }
+}
+
+public class Methods
+{
+    public static void TwoDArrayExample()
+    {
+        TwoDArray d = new TwoDArray();
+        d[1, 2] = 123;
+        Console.WriteLine(d[1, 2]);
+    }
+    public static void DataTableExample()
+    {
+        DataTable Table = new();
+        Table.Columns.Add(new DataColumn("First Name"));
+        Table.Columns.Add(new DataColumn("Last Name"));
+        Table.Columns.Add(new DataColumn("Age"));
+        Table.Rows.Add("Mohd Aman", "Shah", 23);
+        Console.WriteLine(Table.Rows[0][0]);
+        Console.WriteLine(Table.Rows[0][1]);
+        Console.WriteLine(Table.Rows[0][2]);
+    }
+    public static void Carpet()
+    {
+        Person p = new Person("Ron", 24);
+        Console.WriteLine(p["Name"]);
+        Console.WriteLine(p["Age"]);
+        try
+        {
+            Console.WriteLine(p["SomeThing"]);
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+            Console.WriteLine("Invalid Index");
+            Console.WriteLine(ex);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            Console.WriteLine(ex.TargetSite);
+        }
+    }
+}
+
+public class TodoList
+{
+    private ArrayList Items = new ArrayList();
+    public TodoList() { }
+    public TodoItem this[int index]
+    {
+        get { return (TodoItem)Items[index]; }
+        set
+        {
+            if (index < Items.Count && index >= 0)
+                Items[index] = value;
+            else
+            {
+                Items.Add(value);
+            }
+        }
+    }
+}
+
+public class TodoItem
+{
+    public string Title { get; set; }
+    public bool Completed { get; set; }
+    public TodoItem(string title, bool completed)
+    {
+        Title = title;
+        Completed = completed;
+    }
+    public override string ToString()
+    {
+        return $"[Title: {Title}; Completed: {Completed}]";
+    }
+}
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+    public override string ToString()
+    {
+        return $"[Name: {Name}; Age: {Age}]";
+    }
+    public object this[string index]
+    {
+        get
+        {
+            return index switch
+            {
+                "Name" => Name,
+                "Age" => Age,
+                _ => throw new ArgumentOutOfRangeException(nameof(index)),
+            };
+        }
+    }
+}
+
+class TwoDArray
+{
+    private int[,] Data = new int[10, 10];
+    public int this[int row, int col]
+    {
+        get
+        {
+            return Data[row, col];
+        }
+        set
+        {
+            Data[row, col] = value;
+        }
+    }
+}
---
+++ chapter11/OverloadingOperators/Program.cs
@@ -0,0 +1,104 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Point p1 = new Point(1, 2);
+        Point p2 = new Point(1, 2);
+        Point p3 = p1 + p2;
+        Point p4 = 5 + p3;
+        Point p5 = p4 + 5;
+        Console.WriteLine(p1);
+        Console.WriteLine(p2);
+        Console.WriteLine(p3);
+        Console.WriteLine(p4);
+        Console.WriteLine(p5);
+        Console.WriteLine(p1 - p5);
+        p3 += p4;
+        Console.WriteLine("p3: {0}", p3);
+        Console.WriteLine("p3++: {0}", p3++);
+        Console.WriteLine("p3: {0}", p3);
+        Console.WriteLine("++p3: {0}", ++p3);
+        Console.WriteLine(p1 == p2);
+        Console.WriteLine(p1 < p2);
+        Console.WriteLine(p1 <= p2);
+    }
+}
+
+class Point : IComparable<Point>
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+    public override string ToString()
+    {
+        return $"[{X}, {Y}]";
+    }
+    public static Point operator +(Point p1, Point p2)
+    {
+        return new Point(p1.X + p2.X, p1.Y + p2.Y);
+    }
+    public static Point operator -(Point p1, Point p2)
+    {
+        return new Point(p1.X - p2.X, p1.Y - p2.Y);
+    }
+    public static Point operator +(Point p1, int change)
+    {
+        return new Point(p1.X + change, p1.Y + change);
+    }
+    public static Point operator +(int change, Point p)
+    {
+        return p + change;
+    }
+    public static Point operator ++(Point p)
+    {
+        return new Point(p.X + 1, p.Y + 1);
+    }
+    public override bool Equals(object? obj)
+    {
+        return this.ToString() == obj?.ToString();
+    }
+    public override int GetHashCode()
+    {
+        return this.ToString().GetHashCode();
+    }
+
+    public int CompareTo(Point? other)
+    {
+        if (other is null) return 1;
+        double disp1 = Math.Sqrt(Math.Pow((double)X, 2.0) + Math.Pow((double)Y, 2.0));
+        double disp2 = Math.Sqrt(Math.Pow((double)other.X, 2.0) + Math.Pow((double)other.Y, 2.0));
+        if (disp1 > disp2) return 1;
+        else if (disp1 == disp2) return 0;
+        return -1;
+    }
+
+    public static bool operator ==(Point p1, Point p2)
+    {
+        return (p1.X == p2.X && p1.Y == p2.Y);
+    }
+    public static bool operator !=(Point p1, Point p2)
+    {
+        return (p1.X != p2.X || p1.Y != p2.Y);
+    }
+    public static bool operator <(Point p1, Point p2)
+    {
+        return p1.CompareTo(p2) < 0;
+    }
+    public static bool operator >(Point p1, Point p2)
+    {
+        return p1.CompareTo(p2) > 0;
+    }
+    public static bool operator <=(Point p1, Point p2)
+    {
+        return p1.CompareTo(p2) <= 0;
+    }
+    public static bool operator >=(Point p1, Point p2)
+    {
+        return p1.CompareTo(p2) >= 0;
+    }
+
+}
