4f1207c7b57c8ff5d9e7fd58861b9097bfdf071e
Mohd Aman Shah <reach.mohdamanshah@gmail.com>
2024-10-27 06:47:52 +0530

    completed anonymous types

---
+++ chapter11/AnonymousTyps/Program.cs
@@ -0,0 +1,7 @@
+﻿var car = new { Make = "Honda", Color = "White", Speed = "200" };
+Console.WriteLine(car);
+var newCar = new { Make = "Honda", Color = "White", Speed = "200" };
+Console.WriteLine(car == newCar);
+Console.WriteLine(car.GetHashCode());
+Console.WriteLine(newCar.GetHashCode());
+Console.WriteLine(car.Equals(newCar));
