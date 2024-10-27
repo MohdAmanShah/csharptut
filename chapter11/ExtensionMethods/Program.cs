b9e093c6e009cf1cf9be721ced6071d5f054686b
Mohd Aman Shah <reach.mohdamanshah@gmail.com>
2024-10-27 06:05:41 +0530

    completed extension methods

---
+++ chapter11/ExtensionMethods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+int a = 1234567890;
+Console.WriteLine(a);
+Console.WriteLine(a.ReverseDigits());
+static class Extensions
+{
+    public static void DisplayDefiningAssembly(this object obj)
+    {
+        Console.WriteLine("{0} lives here: => {1}\n", obj.GetType().Name, Assembly.GetAssembly(obj.GetType()).GetType().Name);
+    }
+    public static int ReverseDigits(this int i)
+    {
+        int j = 0;
+        while (i > 0)
+        {
+            j *= 10;
+            j += i % 10;
+            i /= 10;
+        }
+        return j;
+    }
+}
