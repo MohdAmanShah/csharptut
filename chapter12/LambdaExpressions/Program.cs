11ea34ac5573ccc8dac5df71695984c1e0ef01fd
Mohd Aman Shah <reach.mohdamanshah@gmail.com>
2024-10-29 05:05:11 +0530

    chapter 12 done

---
+++ chapter12/LambdaExpressions/Program.cs
@@ -0,0 +1,38 @@
+﻿// using System.Security.Cryptography.X509Certificates;
+
+// List<int> list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+// List<int> listEven = list.FindAll(x => x % 2 == 0);
+// List<int> listOdd = list.FindAll(IsOdd);
+// // Predicate<int> predicate1 = new Predicate<int>(IsMultipleOf3);
+// Predicate<int> predicate1 = new Predicate<int>((int x) => x % 2 == 0);
+// List<int> listThrees = list.FindAll(predicate1);
+
+// var dv = delegate (int x) { return x * x; };
+// Console.WriteLine(dv(23));
+
+// foreach (int i in listEven) Console.Write("{0} ", i);
+// Console.WriteLine();
+// foreach (int i in listOdd) Console.Write("{0} ", i);
+// Console.WriteLine();
+// foreach (int i in listThrees) Console.Write("{0} ", i);
+// Console.WriteLine();
+
+// static bool IsOdd(int x)
+// {
+//     return (x % 2 == 1);
+// }
+// static bool IsMultipleOf3(int x)
+// {
+//     return x % 3 == 0;
+// }
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+    }
+    public void Prog()
+    {
+    }
+}
