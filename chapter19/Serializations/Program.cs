bf3a8cc155f9101cd3e86b85dff776acaec7c34b
Mohd Aman Shah <reach.mohdamanshah@gmail.com>
2024-11-30 18:16:50 +0530

    json serialisation

--- chapter19/Serializations/Program.cs
+++ chapter19/Serializations/Program.cs
@@ -2,10 +2,16 @@
 global using System.Text.Json.Serialization;
 global using System.Xml;
 global using System.Xml.Serialization;
+using System.IO.Enumeration;
 
 
 
+Human h1 = new Human() { Name = "John" };
+Human h2 = new Human() { Name = "Rock" };
+h1.Friend = h2;
+h2.Friend = h1;
 
+SaveAsJSONDocument(h1, "human.json");
 
 var theRadio = new Radio
 {
@@ -39,9 +45,56 @@ Person p = new Person
     IsAlive = true
 };
 
-SaveAsXMLDocument(myCars, "myCars.xml");
-SaveAsXMLDocument(p, "person.xml");
+// SaveAsXMLDocument(myCars, "myCars.xml");
+// SaveAsXMLDocument(p, "person.xml");
+// Person s = ReadXML<Person>("person.xml");
+// List<JamesBondCar> cars = ReadXML<List<JamesBondCar>>("myCars.xml");
+// foreach (var car in cars)
+// {
+//     Console.WriteLine(car);
+// }
+// SaveAsJSONDocument(myCars, "myCar.json");
+// SaveAsJSONDocument(p, "person.json");
+// Person h = ReadJSONDocument<Person>("person.json");
+// Console.WriteLine(h);
+await SerializeAsync();
+Console.ReadKey();
+async static Task SerializeAsync()
+{
+    using Stream stream = Console.OpenStandardInput();
+    var data = new { Data = PrintNumbers(3) };
+    await JsonSerializer.SerializeAsync(stream, data);
+}
 
+static async IAsyncEnumerable<int> PrintNumbers(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        yield return i;
+    }
+}
+
+static T ReadJSONDocument<T>(string fileName)
+{
+    using Stream stream = File.OpenRead(fileName);
+    JsonSerializerOptions jsonSerializerOptions = new JsonSerializerOptions()
+    {
+        PropertyNamingPolicy = null
+    };
+    T obj = JsonSerializer.Deserialize<T>(stream, jsonSerializerOptions);
+    return obj;
+}
+static void SaveAsJSONDocument<T>(T objectGraph, string fileName)
+{
+    JsonSerializerOptions jsonSerializerOptions = new JsonSerializerOptions()
+    {
+        IncludeFields = true,
+        WriteIndented = true,
+        ReferenceHandler = ReferenceHandler.Preserve
+        // PropertyNamingPolicy = null
+    };
+    File.WriteAllText(fileName, System.Text.Json.JsonSerializer.Serialize(objectGraph, jsonSerializerOptions));
+}
 static void SaveAsXMLDocument<T>(T objectGraph, string fileName)
 {
     XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
@@ -51,11 +104,23 @@ static void SaveAsXMLDocument<T>(T objectGraph, string fileName)
     }
 }
 
+static T ReadXML<T>(string fileName)
+{
+    XmlSerializer serializer = new XmlSerializer(typeof(T));
+    using FileStream fs = File.OpenRead(fileName);
+    T obj = (T)serializer.Deserialize(fs);
+    return obj;
+}
 public class Radio
 {
+    [JsonInclude]
+    [JsonPropertyOrder(1)]
     public bool HasTweeters;
+    [JsonInclude]
     public bool HasSubWoofers;
+    [JsonInclude]
     public List<double> StationPresets;
+    [JsonInclude]
     public string RadioId = "XF-552RR6";
     public override string ToString()
     {
@@ -66,7 +131,9 @@ public class Radio
 
 public class Car
 {
+    [JsonInclude]
     public Radio TheRadio = new Radio();
+    [JsonInclude]
     public bool IsHatchBack;
     public override string ToString()
     => $"IsHatchback:{IsHatchBack} Radio:{TheRadio.ToString()}";
@@ -75,9 +142,9 @@ public class Car
 
 public class JamesBondCar : Car
 {
-    [XmlAttribute]
+    [JsonInclude]
     public bool CanFly;
-    [XmlAttribute]
+    [JsonInclude]
     public bool CanSubmerge;
     public override string ToString()
     => $"CanFly:{CanFly}, CanSubmerge:{CanSubmerge} {base.ToString()}";
@@ -87,6 +154,7 @@ public class JamesBondCar : Car
 public class Person
 {
     // A public field.
+    [JsonInclude]
     public bool IsAlive = true;
     // A private field.
     private int PersonAge = 21;
@@ -101,3 +169,10 @@ public class Person
     public override string ToString() =>
     $"IsAlive:{IsAlive} FirstName:{FirstName} Age:{PersonAge} ";
 }
+
+public class Human
+{
+    public string Name { get; set; }
+    public Human Friend { get; set; }
+}
+
